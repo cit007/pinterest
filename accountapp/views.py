0dd7dca38b09277397a3e812d7c6bd16dbbf9c00
Intack Cho <intackcho@IntacknoMacBook-Pro15.local>
2021-03-03 22:52:03 +0900

    post data to database. show model data

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
     if request.method == "POST":
         input_msg = request.POST.get("hello_world_input")
 
-        return render(request, 'accountapp/hello_world.html', context={'text': input_msg})
+        # test model
+        new_hello_world = HelloWorld()
+        new_hello_world.text = input_msg
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
