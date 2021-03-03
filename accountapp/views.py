b417e52128c2b8bf5ab43ff0dc71a4b1ae105125
Intack Cho <intackcho@IntacknoMacBook-Pro15.local>
2021-03-04 00:41:12 +0900

    redirect

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 # Create your views here.
 from accountapp.models import HelloWorld
+from django.urls import reverse
 
 
 def hello_world(request):
@@ -14,12 +15,8 @@ def hello_world(request):
         new_hello_world.text = input_msg
         new_hello_world.save()
 
-        # get data all from HelloWorld
-        hello_world_list = HelloWorld.objects.all()
-
-        # for item in hello_world_list:
-        #     print(f"item: {item.text}")
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        # redirect account/hello_world using accountapp of urls.py
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
