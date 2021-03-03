7147e366937bac87e19f143b531766da5d853f14
Intack Cho <intackcho@IntacknoMacBook-Pro15.local>
2021-03-03 11:31:05 +0900

    templates

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 
 def hello_world(request):
-    return HttpResponse('hello world')
+    return render(request, 'base.html')
