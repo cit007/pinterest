bd7f0bd56946852a2d9950374aaf72f44ef76a74
Intack Cho <intackcho@IntacknoMacBook-Pro15.local>
2021-03-04 16:02:01 +0900

    Detail

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 from accountapp.models import HelloWorld
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 
 def hello_world(request):
@@ -29,3 +29,7 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    template_name = 'accountapp/detail.html'
