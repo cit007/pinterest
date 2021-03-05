9927e0985c0e08747262778ec6f61dee49e8dc07
Intack Cho <intackcho@IntacknoMacBook-Pro15.local>
2021-03-05 15:40:19 +0900

    create profile. @See ProfileCreateView-form_valid temp_profile

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from profileapp.views import ProfileCreateView
+
 app_name = "profileapp"
 
 urlpatterns = [
-    #path('create/', xxx.as_view(), name='create'),
+    path('create/', ProfileCreateView.as_view(), name='create'),
 ]
