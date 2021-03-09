3d7f06af4659a447fe2ef633f08133d68199af0f
Intack Cho <intackcho@IntacknoMacBook-Pro15.local>
2021-03-09 16:33:53 +0900

    article update

--- articleapp/views.py
+++ articleapp/views.py
@@ -4,10 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from articleapp.models import Article
 from articleapp.forms import ArticleCreationForm
+
+from articleapp.decorators import article_custom_ownership_required
+
+has_ownership = [login_required, article_custom_ownership_required]
 
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
@@ -26,7 +30,20 @@ class ArticleCreateView(CreateView):
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
+
 class ArticleDetailView(DetailView):
     model = Article
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
