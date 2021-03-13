ed5a49abec89fc09b0ddd842111dcff5ceff5b11
Intack Cho <intackcho@IntacknoMacBook-Pro15.local>
2021-03-14 01:28:12 +0900

    create article without selecting project

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -10,6 +10,9 @@ class ArticleCreationForm(ModelForm):
     content = forms.CharField(
         widget=forms.Textarea(attrs={'class': 'editable'}))
 
+    project = forms.ModelChoiceField(
+        queryset=Project.objects.all(), required=False)
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'content', 'project']
