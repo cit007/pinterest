8d9627de92178cdd5063444240e767bed787f77f
Intack Cho <intackcho@IntacknoMacBook-Pro15.local>
2021-03-14 01:39:15 +0900

    article creation with project detailed name

--- projectapp/models.py
+++ projectapp/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
 class Project(models.Model):
     writer = models.ForeignKey(
         User, on_delete=models.SET_NULL, related_name='project', null=True)
@@ -10,3 +12,6 @@ class Project(models.Model):
     desc = models.TextField(max_length=200, null=True)
 
     created_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.pk} : {self.title}'
