65531aa24d61c4e02b23b40cc34ca82cdb00d0cf
cit007 <itcho0077@gmail.com>
2021-03-31 02:31:43 +0900

    dev and prod settings

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.9.2
 
 WORKDIR /home/
 
+RUN echo "delete cache for deploy in prod"
+
 RUN git clone https://github.com/cit007/pinterest.git
 
 WORKDIR /home/pinterest/
@@ -10,13 +12,16 @@ RUN pip install -r requirements.txt
 
 RUN pip install gunicorn
 
+RUN pip install mysqlclient
+
 RUN echo "SECRET_KEY=xxx" > .env
 
-RUN python manage.py migrate
+# RUN python manage.py migrate
 
 RUN python manage.py collectstatic
 
 EXPOSE 8000
 
 # CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
-CMD ["gunicorn", "pragmatic.wsgi", "--bind", "0.0.0.0:8000"]
+# CMD ["gunicorn", "pragmatic.wsgi", "--bind", "0.0.0.0:8000"]
+CMD ["bash", "-c", "python manage.py migrate && gunicorn pragmatic.wsgi --env DJANGO_SETTINGS_MODULE=pragmatic.settings.deploy --bind 0.0.0.0:8000"]
--- pragmatic/settings/base.py
+++ pragmatic/settings/base.py
@@ -17,30 +17,8 @@ import os
 from django.contrib import messages
 from django.urls import reverse_lazy
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# reading .env file
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Application definition
 
@@ -94,13 +72,6 @@ WSGI_APPLICATION = 'pragmatic.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -157,22 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 MESSAGE_TAGS = {
     messages.DEBUG: 'dark',
     messages.ERROR: 'danger',
-}
-
-# LOGGING
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    },
 }
